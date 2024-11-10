364c31da17faf715a65ad3c685f03f157d26a414
Rediet Bekele <redu0587@gmail.com>
2024-11-10 17:49:57 +0000

    DONE

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-
 from .models import Book
-
+from django.views.generic import DetailView
+from .models import Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
-from django.views.generic import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
