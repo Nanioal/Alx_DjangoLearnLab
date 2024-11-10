4825aa55ee25aa5fd020fd959827804ce9a8e7d1
Rediet Bekele <redu0587@gmail.com>
2024-11-10 17:53:05 +0000

    Going good

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from .models import Book
 from django.views.generic import DetailView
 from .models import Library
+from django.shortcuts import render
+from .models import Book
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
