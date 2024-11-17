07654c2af6d3faaf0884955c9f4a7d3bb463100e
Rediet Bekele <redu0587@gmail.com>
2024-11-17 07:57:13 +0000

    done

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,21 +7,25 @@ from .forms import BookForm
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book
-from .forms import BookForm
-
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'bookshelf/form_example.html', {'form': form})
+from .forms import ExampleForm
+def add_book(request): 
+    if request.method == 'POST': 
+        form = ExampleForm(request.POST) 
+        if form.is_valid(): form.save() 
+        return redirect('book_list') 
+    else: 
+            form = ExampleForm() 
+            return render(request, 'bookshelf/form_example.html', {'form': form}) 
+def edit_book(request, pk): 
+    book = get_object_or_404(Book, pk=pk) 
+    if request.method == 'POST': 
+        form = ExampleForm(request.POST, instance=book) 
+        if form.is_valid(): 
+            form.save() 
+            return redirect('book_list') 
+        else: 
+            form = ExampleForm(instance=book) 
+            return render(request, 'bookshelf/form_example.html', {'form': form})
 
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
