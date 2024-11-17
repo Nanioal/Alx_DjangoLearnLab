5f8a708e8253ac820197a26a5d95e29f999c788c
Rediet Bekele <redu0587@gmail.com>
2024-11-17 08:04:59 +0000

    done

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from .views import book_list, add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('books/', book_list, name='book_list'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -8,6 +8,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book
 from .forms import ExampleForm
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 def add_book(request): 
     if request.method == 'POST': 
         form = ExampleForm(request.POST) 
