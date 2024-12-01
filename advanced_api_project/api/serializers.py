65a6a8ee24e3e1b4ab97f94b3d2d8015135ee9f8
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-01 17:31:29 +0000

    done

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -41,3 +41,14 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['name', 'books']
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet
+from .views import AuthorViewSet, BookViewSet, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
@@ -8,4 +8,10 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+    path('books/', BookListView.as_view(), name='book-list'), # ListView 
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # DetailView 
+    path('books/new/', BookCreateView.as_view(), name='book-create'), # CreateView 
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book-update'), # UpdateView 
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # DeleteView 
+    ]
+
