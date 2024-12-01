b74bbb1e0a6420dfd356014d8a8a4a87bfb9a306
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-01 17:40:54 +0000

    DOne

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('books/', BookListView.as_view(), name='book-list'), # ListView 
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # DetailView 
-    path('books/new/', BookCreateView.as_view(), name='book-create'), # CreateView 
-    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book-update'), # UpdateView 
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # DeleteView 
+    path('books/new/', BookCreateView.as_view(), name='book/create'), # CreateView 
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book/update'), # UpdateView 
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book/delete'), # DeleteView 
     ]
 
