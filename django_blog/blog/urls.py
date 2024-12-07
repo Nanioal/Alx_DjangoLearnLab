710d3dabcf5adc401626a0e406ad5b2e3883ba89
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-07 21:11:52 +0000

    DOne

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,8 +7,7 @@ from .views import (
     PostDeleteView,
     CommentCreateView,
     CommentUpdateView,
-    CommentDeleteView,
-    add_comment
+    CommentDeleteView
 )
 
 urlpatterns = [
@@ -17,7 +16,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='add-comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Corrected URL for creating a new comment
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
