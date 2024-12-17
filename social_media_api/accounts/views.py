45d2394dec0efcd08b8014416ff2f2f654cee156
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-17 18:06:45 +0000

    DONE

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 # accounts/views.py
 
+# accounts/views.py
+
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import User
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
 class LoginView(ObtainAuthToken):
@@ -17,15 +24,8 @@ class LoginView(ObtainAuthToken):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key})
-# accounts/views.py
 
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-from .models import User
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, user_id):
@@ -33,12 +33,10 @@ class FollowUserView(APIView):
         request.user.following.add(user_to_follow)
         return JsonResponse({"message": "User followed"})
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, user_id):
         user_to_unfollow = get_object_or_404(User, id=user_id)
         request.user.following.remove(user_to_unfollow)
         return JsonResponse({"message": "User unfollowed"})
-
-# Create your views here.
