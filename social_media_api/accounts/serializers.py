f12efc062a44a8de92efbc25d9266afde62e0c5d
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-17 17:11:42 +0000

    done

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,9 +2,13 @@
 
 # accounts/serializers.py
 
+# accounts/serializers.py
+
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
